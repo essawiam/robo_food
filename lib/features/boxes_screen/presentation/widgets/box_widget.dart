bfe6e48e47c312e5ae7c040ebdb2a7df012f9a48
wiam <naamawiam@gmail.com>
2023-09-15 18:48:02 +0200

    Add the timer

--- lib/features/boxes_screen/presentation/widgets/box_widget.dart
+++ lib/features/boxes_screen/presentation/widgets/box_widget.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:robo_food/core/constants/titles.dart';
 import 'package:robo_food/features/boxes_screen/data/models/dish_model.dart';
 
-class BoxWidget extends StatelessWidget {
+import '../../../../core/utils/timer_formater.dart';
+
+class BoxWidget extends StatefulWidget {
   final DishModel? dishModel;
   final int numberBox;
 
@@ -13,9 +17,128 @@ class BoxWidget extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  State<BoxWidget> createState() => _BoxWidgetState();
+}
+
+class _BoxWidgetState extends State<BoxWidget> {
+  late Timer timer;
+  int remainingSeconds = 0;
+  bool isDishPickedUp = false;
+  @override
+  void initState() {
+    super.initState();
+    startTimer(10);
+  }
+
+  void startTimer(int seconds) {
+    remainingSeconds = seconds;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (remainingSeconds > 0) {
+          remainingSeconds--;
+        } else {
+          timer.cancel();
+          _handleTimerEnd();
+        }
+      });
+    });
+  }
+
+  void _handleTimerEnd() {
+    setState(() {
+      // Here if the isDishPickedUp == false after we finish this.
+      // Will retunred it back.
+      // and then ask for a new othor orders.
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return dishModel != null
-        ? Container(
+    return widget.dishModel != null && !isDishPickedUp
+        ? InkWell(
+            onTap: () {
+              setState(() {
+                isDishPickedUp = true;
+              });
+            },
+            child: Container(
+              width: 185,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  width: 10.0,
+                  color: Colors.black26,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+                          child: Image.asset(
+                            widget.dishModel!.imageUrl,
+                            height: 60, // Adjust the height as needed
+                            width: 60, // Adjust the width as needed
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '$boxTitle : ${widget.numberBox.toString()}',
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              '$orderId : ${widget.dishModel!.orderId.toString()}',
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.watch_later,
+                          color: Colors.green,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          formatDuration(Duration(seconds: remainingSeconds)),
+                          style: const TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        : Container(
             width: 185,
             height: 150,
             decoration: BoxDecoration(
@@ -25,72 +148,13 @@ class BoxWidget extends StatelessWidget {
                 color: Colors.black26,
               ),
             ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(30.0),
-                        child: Image.asset(
-                          dishModel!.imageUrl,
-                          height: 60, // Adjust the height as needed
-                          width: 60, // Adjust the width as needed
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '$boxTitle : ${numberBox.toString()}',
-                            style: const TextStyle(
-                              fontSize: 18.0,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                          Text(
-                            '$orderId : ${dishModel!.orderId.toString()}',
-                            style: const TextStyle(
-                              fontSize: 18.0,
-                              color: Colors.black,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Row(
-                    children: [
-                      Icon(
-                        Icons.watch_later,
-                        color: Colors.green,
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        '20:10',
-                        style: TextStyle(
-                          fontSize: 18.0,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
-        : const SizedBox();
+            child: const SizedBox());
+  }
+
+  @override
+  void dispose() {
+    // Cancel the timer when the widget is disposed
+    timer.cancel();
+    super.dispose();
   }
 }
